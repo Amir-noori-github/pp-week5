018308ffd7295e25f24acda9fcde4d8aeaa35012
Mehdi <mehdicontacto@gmail.com>
2025-09-15 15:28:40 +0300

    Implementing Error-Handling Middleware

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
-const { unknownEndpoint } = require("./middleware/customMiddleware");
+const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
 
 const morgan = require("morgan");
@@ -18,8 +18,15 @@ app.use("/api/tours", tourRouter);
 // Use the userRouter for all /users routes
 app.use("/api/users", userRouter);
 
+// Example route that throws an error
+app.get('/error', (req, res, next) => {
+  // Trigger an error
+  const error = new Error("Network problem");
+  next(error);
+});
+
 app.use(unknownEndpoint);
-// app.use(errorHandler);
+app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
 // Start the server
